562533fdd41bec5c1f4efb24da1408681befa51e
Raghav Gade <r29aghavgade@gmail.com>
2023-04-12 10:55:18 +0530

    Client setup --nav added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,59 @@
 import React from "react";
 import "./App.css";
 
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+import { Home, About, Player, Players, Matches, Points, Stats } from "./pages";
+import { Layout } from "./components";
+
 function App() {
-  return <div className="App">Hello</div>;
+  const Navbar = () => {
+    return (
+      <div className="App">
+        <Layout />
+        <Outlet />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/matches",
+          element: <Matches />,
+        },
+        {
+          path: "/player/:id",
+          element: <Player />,
+        },
+        {
+          path: "/players",
+          element: <Players />,
+        },
+        {
+          path: "/points",
+          element: <Points />,
+        },
+        {
+          path: "/stats",
+          element: <Stats />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
