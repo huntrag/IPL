590802ef1d88c6b962b8097be403dae6aaf28500
prasadkukde <103832580+prasadkukde@users.noreply.github.com>
2023-04-14 11:44:35 +0530

    Add files via upload

---
+++ project_tables.sql
@@ -0,0 +1,10 @@
+select * from batting_scorecard;
+select * from bowling_scorecard;
+select * from teams;
+select * from seasons;
+select * from Match_details;
+select * from result;
+select * from Scorecard;
+select * from Player;
+
+
