bb8016773c2b1ede921cb4031a1de31e093dd1ec
Raghav Gade <r29aghavgade@gmail.com>
2023-04-15 13:16:07 +0530

    Match Card in Matches replaced with Featured Match Card

--- client/src/pages/Matches/Matches.jsx
+++ client/src/pages/Matches/Matches.jsx
@@ -3,14 +3,20 @@ import { Grid } from "@mui/material";
 import MatchCard from "../../components/MatchCard/MatchCard";
 
 import data from "../../../../scripts/schedule.json";
+import FeaturedMatchCard from "../../components/FeaturedMatchCard/FeaturedMatchCard";
 
 const Matches = () => {
   return (
-    <Grid container rowSpacing={3}>
+    <Grid
+      container
+      rowSpacing={3}
+      alignItems="center"
+      justifyContent={"flex-start"}
+    >
       {data.map((match, index) => {
         return (
           <Grid item sx={{ m: 2 }}>
-            <MatchCard match={match} />
+            <FeaturedMatchCard match={match} />
           </Grid>
         );
       })}
